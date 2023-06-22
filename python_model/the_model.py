50cbe27f8b3261adcb3a597b57642e37dc35e989
sivadhulipala1999 <parthushiva123@gmail.com>
2023-06-22 08:08:02 +0200

    Updated the code for sampling in python

--- python_model/the_model.py
+++ python_model/the_model.py
@@ -7,17 +7,17 @@ from scipy.stats import norm
 from scipy.special import logit
 import time
 
-bad_data = pd.read_csv("observations_bad.csv")
-bad_data = bad_data.to_numpy()
+"""
+TODO: fix synthetic data
+TODO: check if the parameters match for synthetic, observed data 
+"""
 
 # these parameters are being defined based on the initial setting done in author's code
 theta1_theta2_mu_dict = {"bad": [220, 0.15], "medium": [270, 0.15], "good": [320, 0.15]}
 theta1_theta2_sd_dict = {"bad": [25, 0.03], "medium": [25, 0.03], "good": [25, 0.03]}
 
 # chosen based on the observations made by the authors. Represents the maximum wind speed observed across multiple areas. Can be made so that it is different for different building types
-# norm_factor = 296
-norm_factor = max(bad_data[:, 0])
-y_observed = bad_data[:, 1]
+norm_factor = 300
 
 
 def inv_logit(x):
@@ -49,6 +49,14 @@ def get_mu_sd(lower_mu, prob_lower_mu, upper_mu, prob_upper_mu, building_type):
 
 BUILDING_TYPE = "good"
 
+theta_mus = theta1_theta2_mu_dict[BUILDING_TYPE]
+theta_sds = theta1_theta2_sd_dict[BUILDING_TYPE]
+theta1_mu = theta_mus[0]/norm_factor
+theta2_mu = theta_mus[1]
+theta1_sd = theta_sds[0]/norm_factor
+theta2_sd = theta_sds[1]
+
+# synthetic data prep
 pi_0_params = get_mu_sd(lower_mu=0.01, prob_lower_mu=0.99, upper_mu=0.05, prob_upper_mu=0.01, building_type=BUILDING_TYPE)
 pi_1_params = get_mu_sd(lower_mu=0.95, prob_lower_mu=0.01, upper_mu=0.99, prob_upper_mu=0.99, building_type=BUILDING_TYPE)
 
@@ -65,52 +73,61 @@ theta5_sd = abs(pi_1_params[0]/10)
 theta6_mu = pi_1_params[1]
 theta6_sd = abs(pi_1_params[1]/10)
 
-print(f"theta3 - mu: {theta3_mu}, sd: {theta3_sd}")
-print(f"theta4 - mu: {theta4_mu}, sd: {theta4_sd}")
-print(f"theta5 - mu: {theta5_mu}, sd: {theta5_sd}")
-print(f"theta6 - mu: {theta6_mu}, sd: {theta6_sd}")
+
+print(f"theta 3: mu - {theta3_mu}, sd - {theta3_sd}")
+print(f"theta 4: mu - {theta4_mu}, sd - {theta4_sd}")
+print(f"theta 5: mu - {theta5_mu}, sd - {theta5_sd}")
+print(f"theta 6: mu - {theta6_mu}, sd - {theta6_sd}")
+
+# Define the wind speed values for simulation
+epsilon = 0.001
+
+lower_v = 180
+upper_v = 290
+
+size = 250
+v = []
+
+for _ in range(size):
+    v.append(np.random.randint(lower_v, upper_v))
+
+v = np.array(v)/norm_factor
+
+pi0 = inv_logit(theta3_mu + theta4_mu * np.mean(v))
+pi1 = inv_logit(theta5_mu + theta6_mu * np.mean(v))
+
+# Simulate the damage ratio data
+mu_obs = phi(np.log(np.mean(v/theta1_mu)) * (1/theta2_mu))
+
+y_zero_obs = [np.random.choice([0, 1], p=[pi0, 1 - pi0]) for _ in range(size)]
+y_one_obs = [np.random.choice([0, 1], p=[1 - (1 - pi0) * pi1, (1 - pi0) * pi1]) for _ in range(size)]
+y_beta_obs = np.random.beta(mu_obs + epsilon, (1-mu_obs) + epsilon, size=size)
 
 st = time.time()
 
 with pm.Model():
     # Priors for unknown model parameters
-    theta_mus = theta1_theta2_mu_dict[BUILDING_TYPE]
-    theta_sds = theta1_theta2_sd_dict[BUILDING_TYPE]
-    theta1_mu = theta_mus[0]
-    theta2_mu = theta_mus[1]
-    theta1_sd = theta_sds[0]
-    theta2_sd = theta_sds[1]
 
     theta1 = pm.Normal("theta1", mu=theta1_mu, sigma=theta1_sd)
-    theta2 = pm.Normal("theta2", mu=theta1_mu, sigma=theta1_sd)
+    theta2 = pm.Normal("theta2", mu=theta2_mu, sigma=theta2_sd)
     theta3 = pm.Normal("theta3", mu=theta3_mu, sigma=theta3_sd)
     theta4 = pm.Normal("theta4", mu=theta4_mu, sigma=theta4_sd)
     theta5 = pm.Normal("theta5", mu=theta5_mu, sigma=theta5_sd)
     theta6 = pm.Normal("theta6", mu=theta6_mu, sigma=theta6_sd)
     precision = pm.Uniform("precision", 0, 100) # based on the original code
 
-    v = pm.Uniform("wind_speeds", 180, 290) # assume this distribution based on the data available in the observation files
-
     # Expected value of outcome
     PI_0 = pm.Deterministic("PI_0", pm.math.invlogit(theta3 + theta4 * v))
     PI_1 = pm.Deterministic("PI_1", pm.math.invlogit(theta5 + theta6 * v))
-    mu = 0.5 * (1 + tt.erf((tt.log(v/theta1)/theta2 - 0)/np.sqrt(2)))
-
-    epsilon = 0.000001
+    # mu = 0.5 * (1 + tt.erf((tt.log(v/theta1)/theta2 - 0)/np.sqrt(2)))
+    mu = pm.Deterministic("mu", pm.math.invprobit((1/theta2_mu) * pm.math.log(v/theta1_mu)))
 
     # Likelihood (sampling distribution) of observations
-    y_zero = pm.Uniform.dist( 0, epsilon)
-    y_one = pm.Uniform.dist(1-epsilon, 1)
-    y_beta = pm.Beta.dist(alpha=precision * mu, beta=(1 - mu) * precision)
+    y_zero = pm.Bernoulli("y_zero", p=PI_0,  observed=y_zero_obs)
+    y_one = pm.Bernoulli("y_one", p=PI_1, observed=y_one_obs)
+    y_beta = pm.Beta("y_beta", alpha=mu * precision, beta=(1 - mu) * precision, observed=y_beta_obs)
 
-    weights = [PI_0, (1 - PI_0) * PI_1, (1 - PI_0) * (1 - PI_1)]
-    dists = [y_zero, y_one, y_beta]
-
-    mixture = pm.Mixture("mixture", w=weights, comp_dists=dists)
-
-    y_like = pm.Bernoulli("y_like", p=mixture, observed=y_observed)
-
-    trace = pm.sample(10000, tune=1000, chains=3, cores=1) # tuned samples are being discarded
+    trace = pm.sample(1500, tune=1000, chains=3, cores=1) # tuned samples are being discarded
 
 et = time.time()
 
